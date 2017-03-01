a982d722fcf181e6daf89ba1700b04e44639f5f1
forseyear <HsAKjnD8SuLk7E3caSGhA@gmail.com>
2017-03-02 00:39:49 +0900

    fixed removeAds #1

--- files/removeAds.js
+++ files/removeAds.js
@@ -10,7 +10,6 @@
 			case 'show':
 				object.css('overflow', object.data('orgOverflow'));
 				object.css('margin', object.data('orgMargin'));
-				object.css('padding', object.data('orgPadding'));
 				object.css('width', object.data('orgWidth'));
 				object.css('height', object.data('orgHeight'));
 				break;
@@ -18,61 +17,25 @@
 			case 'hide':
 				object.data('orgOverflow', object.css('overflow'));
 				object.data('orgMargin', object.css('margin'));
-				object.data('orgPadding', object.css('padding'));
 				object.data('orgWidth', object.css('width'));
 				object.data('orgHeight', object.css('height'));
 				object.css('overflow', 'hidden');
 				object.css('margin', 0);
-				object.css('padding', 0);
 				object.css('width', 0);
 				object.css('height', 0);
 				break;
 		}
 	}
-	var changeElements = function(method) {
-		// 綺麗な方法ではないが、エラー対策
-		switch (method) {
-			case 'show':
-				var usersFrameNext = $("#online_users_frame").next();
-				$("#online_users_frame").insertBefore($("#feeder_links_frame")).append($("#online_users"));
-				$("#feeder_links_frame").insertBefore(usersFrameNext).append($("#feeder_links"));
-				break;
-			
-			case 'hide':
-				var linksFrameNext = $("#feeder_links_frame").next();
-				$("#feeder_links_frame").insertBefore($("#online_users_frame")).append($("#online_users"));
-				$("#online_users_frame").insertBefore(linksFrameNext).append($("#feeder_links"));
-				break;
-		}
-	}
 	var toggleAds = function(mode) {
-		// 上部の広告他
-		for (var i=0; i<$('#wrapper').children().length; i++) {
-			var e = $($('#wrapper').children().get(i));
-			if (e.attr('id')) {
-				break;
-			}
-			doaction(e, mode);
-		}
-		
-		// 下部の広告
-		if (!$('#footer').prev().attr('id')) {
-			doaction($('#footer').prev(), mode);
-		}
-		
-		// 右上の広告
-		doaction($('#ad_square'), mode);
-		
-		// 右側の広告
-		doaction($('#ad_skyscraper'), mode);
+		$('.admax-banner').each(function() {
+			doaction($(this).parent(), mode);
+		});
 	};
 	return {
 		'constructor': function() {
-			changeElements('hide');
 			toggleAds('hide');
 		},
 		'destructor': function() {
-			changeElements('show');
 			toggleAds('show');
 		},
 	};
